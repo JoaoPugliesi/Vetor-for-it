80e67470b019c18dc244b57da59a124bb8216cc9
João <joaopaulo.allex@gmail.com>
2022-03-21 21:08:40 -0300

    Projeto acadêmico

---
+++ src/VetorForIt.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class VetorForIt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double v[] = {3.5, 2.75, 9, -4.5};
+		Arrays.sort(v);
+		for(double valor: v) {
+			System.out.println(valor + " ");
+		}
+		
+	
+	}
+}
